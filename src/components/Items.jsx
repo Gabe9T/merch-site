873a2f703e7fec6b55ccb47e6a0c6dee0e9a690d
kimmykokonut <kimmykokonut@gmail.com>
2023-12-07 13:34:26 -0800

    styled img to be consistent size and button to not be a block
    Co-authored-by: Co-authored-by:  Ravin Fisher  <ravesterrr@gmail.com>
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,16 +1,26 @@
 import PropTypes from 'prop-types';
-
 const Items = (props) => {
+    const styleImg = {
+        width: '20rem',
+        height: '20rem'
+    }
+const buttonStyle = {
+
+    width: '15%'
+}
+
     return(
         <>
         <p>Category: {props.category}</p>
-        <img src={props.imgSrc} alt={props.imgAlt}/>
+        <img src={props.imgSrc} alt={props.imgAlt} style={styleImg}/>
         <p>Description: {props.description}</p>
         <p>Price: {props.price}</p>
         <p>Quantity: {props.qty}</p>
         {/* <p>Id: {props.id}</p> */}
+       <div style={buttonStyle}>
         <button>Buy</button> 
         <button>Restock</button>
+        </div>
         </>
     )
 }
